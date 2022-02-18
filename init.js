e1a2cd42ca442c4b8d26e811c686354a525f571b
Taigo Ito <webmaster@qwel.design>
2022-02-18 20:51:16 +0900

    Build the theme for Koshinokuni

---
+++ init.js
@@ -0,0 +1,10 @@
+// Words Flip
+
+const words = document.querySelectorAll('.hero__animate');
+let count = 0;
+words[0].classList.add('animate');
+setInterval(() => {
+  words[count % 3].classList.remove('animate');
+  count++;
+  words[count % 3].classList.add('animate');
+}, 4000);
--- js/init.js
+++ js/init.js
@@ -4,3 +4,14 @@
  * Location: Fukui, Japan
  * @package Qwel-Two
  */
+
+// Words Flip
+
+const words = document.querySelectorAll('.hero__animate');
+let count = 0;
+words[0].classList.add('animate');
+setInterval(() => {
+  words[count % 3].classList.remove('animate');
+  count++;
+  words[count % 3].classList.add('animate');
+}, 4000);
